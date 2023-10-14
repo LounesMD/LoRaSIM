3471a650879eb29812014fe9012b43cfde468c4d
LounesMD <84915434+LounesMD@users.noreply.github.com>
2023-10-14 16:39:23 +0200

    Blockchain Comments
    Comments about the Blocks and the Blockchain methods/classes

--- src/Simulator_WithBlockchain/Block.py
+++ src/Simulator_WithBlockchain/Block.py
@@ -10,7 +10,31 @@ import hashlib
     
 
 class Block(object):
-    
+    """
+    A class representing a block in a blockchain.
+
+    Attributes:
+        data (list): The data contained in the block.
+        merkleroot (str): The Merkle root hash of the block's data.
+        miner (str): The miner's identifier.
+        timestamp (datetime): The timestamp of when the block was created.
+        index (int): The index of the block in the blockchain.
+        dataProvider: The data provider associated with the block.
+        gamma: The gamma value associated with the block.
+
+    Methods:
+        getGamma(self): Returns the gamma value associated with the block.
+        getDataProvider(self): Returns the data provider associated with the block.
+        getMerkleRoot(self): Returns the Merkle root hash of the block's data.
+        getTimeStamp(self): Returns the timestamp of the block.
+        getIndex(self): Returns the index of the block.
+        getMiner(self): Returns the miner's identifier.
+        getData(self): Returns the data contained in the block.
+        setMerkleRoot(self, data): Sets the Merkle root hash of the block's data.
+
+    Note:
+        - The Merkle root hash is calculated based on the block's data.
+    """    
     def __init__(self, index, timestamp, miner, data, dataProvider , gamma):
         self.data = data
         self.merkleroot = self.setMerkleRoot(data)
@@ -67,6 +91,23 @@ class Block(object):
                 
                 
 class BlockUtil(Block):
+    """
+    A utility class for working with blockchain blocks.
+
+    This class extends the Block class and includes additional methods for calculating the block's hash
+    and accessing the previous block.
+
+    Attributes:
+        previousBlock (Block): The previous block in the blockchain.
+        hash (str): The hash value of the current block.
+
+    Methods:
+        getHash(self): Returns the hash value of the block.
+        getPreviousBlock(self): Returns the previous block in the blockchain.
+
+    Note:
+        - The hash of the block is calculated based on various block attributes.
+    """
     def __init__(self, index, timestamp, miner, previousBlock, data,dataProvider , gamma):
         super().__init__(index, timestamp, miner, data,dataProvider,gamma)
         self.previousBlock = previousBlock #index-1 if index>0
@@ -84,6 +125,20 @@ class BlockUtil(Block):
 
                 
 class GenesisBlock(Block):
+    """
+    A specialized class representing the genesis block of a blockchain.
+
+    This class extends the Block class and is specifically designed for the first block in the blockchain.
+
+    Attributes:
+        hash (str): The hash value of the genesis block.
+
+    Methods:
+        getHash(self): Returns the hash value of the genesis block.
+
+    Note:
+        - The hash of the genesis block is calculated based on its attributes.
+    """
     def __init__(self, timestamp, miner, data,dataProvider,gamma):
         super().__init__(0, timestamp, miner, data,dataProvider,gamma)
         m = hashlib.sha256()   
@@ -97,4 +152,14 @@ class GenesisBlock(Block):
         
 
 def sameBlocks(bloc1 , bloc2):
+    """
+    Check if two blocks are the same based on their hash values.
+
+    Args:
+        bloc1 (Block): The first block to compare.
+        bloc2 (Block): The second block to compare.
+
+    Returns:
+        bool: True if the blocks have the same hash value, False otherwise.
+    """
     return (bloc1.getHash() == bloc2.getHash())
--- src/Simulator_WithBlockchain/Blockchain.py
+++ src/Simulator_WithBlockchain/Blockchain.py
@@ -10,16 +10,51 @@ from Block import *
 import time
 
 class Blockchain:    
+    """
+    A class representing a simple PoI blockchain.
+
+    Attributes:
+        blocks (list): A list of blocks in the blockchain.
+        gamma (function): A function representing the threshold as a function of time.
+
+    Methods:
+        __init__(self, threshold): Initializes a new blockchain with the specified threshold function.
+        generateGenesisBlock(self, dataProvider, time): Generates and adds the genesis block to the blockchain.
+        addBlock(self, block): Adds a new block to the blockchain if it follows the correct order.
+        getBlock(self, index): Retrieves a block at the specified index.
+        getBlockChain(self): Retrieves the entire blockchain.
+        getBCSize(self): Returns the current size of the blockchain.
+        printBlock(self, index): Prints information about a specific block in the blockchain.
+    """
     def __init__(self , threshold):
+        """
+        Initializes a new blockchain.
+
+        Args:
+            threshold: A function representing the threshold as a function of time.
+        """
         self.blocks = []
-        self.gamma = threshold #une fonction du temps
+        self.gamma = threshold
         #self.generateGenesisBlock()
         
     def generateGenesisBlock(self,dataProvider,time):
+        """
+        Generates and adds the genesis block to the blockchain.
+
+        Args:
+            dataProvider: The data provider for the genesis block.
+            time: The current time for the genesis block.
+        """
         if(len(self.blocks)==0):
             self.blocks.append(GenesisBlock(datetime.datetime.now(), 'BlockChain', ['Genesis Block'] , dataProvider , self.gamma(time)))
     
     def addBlock(self , block):
+        """
+        Adds a new block to the blockchain if it follows the correct order.
+
+        Args:
+            block: The block to be added to the blockchain.
+        """
         if(len(self.blocks)==0): 
             if(block.getIndex()==0):
                 self.blocks.append(block)
@@ -29,26 +64,44 @@ class Blockchain:
             print("the order is not respected")
         
     def getBlock(self , index):
+        """
+        Retrieves a block at the specified index.
+
+        Args:
+            index (int): The index of the block to retrieve.
+
+        Returns:
+            Block: The block at the specified index.
+        """
         if(0<=index<(len(self.blocks))):
             return self.blocks[index]
 
     def getBlockChain(self):
+        """
+        Retrieves the entire blockchain.
+
+        Returns:
+            list: A list of all blocks in the blockchain.
+        """
         return self.blocks
     
     def getBCSize(self):
+        """
+        Returns the current size of the blockchain.
+
+        Returns:
+            int: The number of blocks in the blockchain.
+        """
         return len(self.blocks)
     
     def printBlock(self, index:int):
-            if(0<=index<=(len(self.blocks))-1):
-                block = self.getBlock(index)
-                print(f"In the block {block.getIndex()}, the informations are : \n timestamp : {block.getTimeStamp()} \n index : {block.getIndex()} \n miner : {block.getMiner()} \n data : {block.getData()} \n DataProvider : {block.dataProvider} \n Gamma(t) : {block.getGamma()}")
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+        """
+        Prints information about a specific block in the blockchain.
+
+        Args:
+            index (int): The index of the block to print.
+        """                        
+        if(0<=index<=(len(self.blocks))-1):
+            block = self.getBlock(index)
+            print(f"In the block {block.getIndex()}, the informations are : \n timestamp : {block.getTimeStamp()} \n index : {block.getIndex()} \n miner : {block.getMiner()} \n data : {block.getData()} \n DataProvider : {block.dataProvider} \n Gamma(t) : {block.getGamma()}")
+                                    
